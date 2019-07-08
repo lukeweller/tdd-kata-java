a124cd7a4db666ba0d5dae04c85dcdaeb44bef6d
Luke Weller <lukeweller8@gmail.com>
2019-07-08 15:36:19 -0500

    Post-CR Refactoring

--- src/tennisKataPackage/GameOverException.java
+++ src/tennisKataPackage/GameOverException.java
@@ -3,9 +3,29 @@ package tennisKataPackage;
 public class GameOverException extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
+	
+	GameOverException()
+	{
+		super();
+	}
 
 	GameOverException(String errorMessage)
 	{
         super(errorMessage);
     }
+	
+	GameOverException(Throwable cause)
+	{
+		super(cause);
+	}
+	
+	GameOverException(String errorMessage, Throwable cause)
+	{
+		super(errorMessage, cause);
+	}
+
+	GameOverException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+	{
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 }
